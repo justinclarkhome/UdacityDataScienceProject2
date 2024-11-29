82bc05cf0e2bfe85a0dbcaae65e3584f4083716a
Justin Clark <justin.clark.git@gmail.com>
2024-11-29 08:38:22 -0500

    added dupe check, customizing main

--- data/process_data.py
+++ data/process_data.py
@@ -8,6 +8,26 @@ import re
 
 DATA_DIR = './'
 
+
+def check_and_drop_duplicates(df):
+    """ Check for duplicates in df - including the index value - and drop if found.
+
+    This function resets the index first, as the Pandas duplicate check is only on values.
+    We want to include the index in the check (the id, which will be the primary 
+    key in the database) as more than 1 observation could have the same characteristics.
+
+    Args:
+        df (DataFrame): DataFrame to check for duplicates/
+
+    Returns:
+        DataFrame: DataFrame with dupes dropped (if any).
+    """
+    if df.reset_index().duplicated().any():
+        print('... duplicates detected: they will be dropped.')
+        df = df.reset_index().drop_duplicates().set_index('id')
+    return df
+
+
 def load_categories_data(categories_filepath, skip_rows=1):
     """ Parse the CSV file containing category information.
 
@@ -16,51 +36,54 @@ def load_categories_data(categories_filepath, skip_rows=1):
         skip_rows (int, optional): Number of rows to skip when parsing (e.g. for the header). Defaults to 1.
 
     Returns:
-        _type_: _description_
+        DataFrame: DataFrame of ints (0 or 1) with categories as columns and 'id' as index.
     """
     data = {}
     print(f'Parsing {categories_filepath}.')
     with open(categories_filepath, 'r') as f:
         for i, line in enumerate(f.readlines()):
-            if i > skip_rows - 1: # remember: Python indexes are 0-based
-                this_line_info = {} # holder for just this line's info
+            if i > skip_rows - 1: # Remember: Python indexes are 0-based!
+                this_line_info = {} # Holder for just this line's info.
                 identifier, keyvalue = line.split(',')
                 for item in keyvalue.split(';'):
                     k, v = item.strip().split('-')
                     this_line_info[k] = v
                 data[int(identifier)] = this_line_info
-    data = pd.DataFrame(data).T.rename_axis('id', axis=0) # name the index, so we can join on it later
+
+    # Convert to dtype int and name the index (so we can join on it later).
+    data = pd.DataFrame(data).T.rename_axis('id', axis=0).astype(int)
+    data = check_and_drop_duplicates(df=data)
     print('... finished!')
     return data
 
 
 def load_messages_data(messages_filepath, skip_rows=1):
-    """_summary_
+    """ Parse the CSV file containing message information.
+
+    Each line has a vaguely consistent format, something like:
+    - 'id', 'message' (in english), 'original' (message in original language), 'genre'.
+    - 'id' is always at the beginning, always a number, and always followed by a comma.
+    - 'genre' is always at the end, and always preceded by a comma.
+    - The rest is harder to parse, because 'original' is sometimes blank (in between two commas, followed by 'genre').
+    - Sometimes 'message' is surrounded in quotes. It LOOKs like it gets surrounded in quotes if it contains a comma.
+
+    While it is difficult to separate 'message' and 'original' when both are present, in the ETL stage we will
+    leave both as one string. Then in the ML pipeline we can try to clean the string with NLTK, to strip
+    out non-English words as best as possible.
 
     Args:
-        messages_filepath (_type_): _description_
-        skip_rows (int, optional): _description_. Defaults to 1.
+        messages_filepath (str): Path to CSV file containing messages info.
+        skip_rows (int, optional): Number of rows to skip when parsing (e.g. for the header). Defaults to 1.
 
     Returns:
-        _type_: _description_
+        DataFrame: DataFrame of strings with columns 'message' and 'genre' and 'id' as index.
     """
     data = {}
     print(f'Parsing {messages_filepath}.')
     with open(messages_filepath, 'r') as f:
         for i, line in enumerate(f.readlines()):
-            if i > skip_rows - 1: # remember: Python indexes are 0-based
-                line = line.strip() # get rid any leading/trailing whitespace, line endings, etc
-
-                # each line has a vaguely consistent format, something like
-                # id, message (in english), original (message in original language), genre
-                # id is always at the beginning, always a number, and always followed by a comma
-                # genre is always as the end, and always preceded by a comma
-                # the rest is harder, because the original is sometimes blank (in between two commas, followed by the genre)
-                # sometimes message is surrounded in quotes, sometimes not. It LOOKs like it gets surrounded in quotes if it contains a comma.
-                # so we can try to apply a regex looking pairs of quotes with a comma in middle
-                # we can also look for two consecutive commas followed by the genre to isolate instances where the original is blank
-
-                # I'm not sure there is a way to split out the english/non-english sections. And maybe it is BETTER not to try, in case there is extra predictive content in there?
+            if i > skip_rows - 1: # Remember: Python indexes are 0-based!
+                line = line.strip() # Get rid any leading/trailing whitespace, line endings, etc.
 
                 # the idenififier is always a number, always at the beginning, and always followed by a comma
                 identifier = re.search(r'^[0-9]+,', line).group() # .replace(',', '')
@@ -86,6 +109,7 @@ def load_messages_data(messages_filepath, skip_rows=1):
                 #     message = remaining.strip('"')
 
     data = pd.DataFrame.from_dict(data).T.rename_axis('id', axis=0) # name the index, so we can join on it later
+    data = check_and_drop_duplicates(df=data)
     print('... finished!')
     return data
 
@@ -111,6 +135,9 @@ def load_data(messages_filepath, categories_filepath):
 
 
 def clean_data(df):
+
+    # for removing non-English words:
+    # https://stackoverflow.com/questions/41290028/removing-non-english-words-from-text-using-python
     pass
 
 
@@ -118,7 +145,29 @@ def save_data(df, database_filename):
     pass  
 
 
-def main():
+def main(
+        messages_filepath = os.path.join(DATA_DIR, 'disaster_messages.csv'),
+        categories_filepath = os.path.join(DATA_DIR, 'disaster_categories.csv'),
+        database_filepath = os.path.join(DATA_DIR, 'project_data.sqlite'),
+):
+    """_summary_
+
+    Args:
+        messages_filepath (_type_, optional): _description_. Defaults to os.path.join(DATA_DIR, 'disaster_messages.csv').
+        categories_filepath (_type_, optional): _description_. Defaults to os.path.join(DATA_DIR, 'disaster_categories.csv').
+        database_filepath (_type_, optional): _description_. Defaults to os.path.join(DATA_DIR, 'project_data.sqlite').
+    """
+
+    # Load raw source data and combine.
+    df = load_data(messages_filepath, categories_filepath)
+    
+    # Clean combined data.
+
+    # Save cleaned data to on-disk database.
+    
+
+
+def main_example():
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
